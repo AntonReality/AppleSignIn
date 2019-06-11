e76ca18212d2f7dfc2da7d7a5f856781628f8345
Anton Vizgin <anton.vizgin@n-cube.co.uk>
2019-06-11 17:55:02 +0300

    Initial Commit

---
+++ AppleSignIn/AppleSignIn.h
@@ -0,0 +1,19 @@
+//
+//  AppleSignIn.h
+//  AppleSignIn
+//
+//  Created by Anton Vizgin on 6/11/19.
+//  Copyright © 2019 Apple Sign In. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppleSignIn.
+FOUNDATION_EXPORT double AppleSignInVersionNumber;
+
+//! Project version string for AppleSignIn.
+FOUNDATION_EXPORT const unsigned char AppleSignInVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleSignIn/PublicHeader.h>
+
+
